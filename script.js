87c4426d4eabd96418423cf2e2384859919df556
alfieBigmanual <alfiemorris567@outlook.com>
2024-05-08 09:40:48 +0100

    Added the JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeBackgroundColour(colour) {
+    document.body.style.backgroundColor = colour;
+  }
+  
+  function displayWelcome(){
+    alert("Welcome to my page!")
+  }
+  
+  
+  
+  
+  
+  
