8fda84f02d0cf0ea891cc49037f9a2a3c5bb5a4b
alfieBigmanual <alfiemorris567@outlook.com>
2024-05-08 11:45:54 +0100

    adding the test files

---
+++ testing.js
@@ -0,0 +1,13 @@
+function changeBackgroundColour(colour) {
+    document.body.style.backgroundColor = colour;
+  }
+  
+  function displayWelcome(){
+    alert("Howdy Ho there gamer you need to sign the season pass!")
+  }
+  
+  
+  
+  
+  
+  
